653aaa5f4843d8d02665b03a888278c149966ad2
lattecatte <154484150+lattecatte@users.noreply.github.com>
2024-06-25 14:33:05 +0900

    Fix var naming and move FUEL_THRESHOLD outside main update func

--- fuel_level_failsafe.lua
+++ fuel_level_failsafe.lua
@@ -1,24 +1,24 @@
 local MAV_SEVERITY = {EMERGENCY=0, ALERT=1, CRITICAL=2, ERROR=3, WARNING=4, NOTICE=5, INFO=6, DEBUG=7}
 local FLIGHT_MODE = {MANUAL=0, AUTO=10, RTL=11, QSTABILIZE=17, QLAND=20, QRTL=21}
-
-local fuel_is_enough = true
+local FUEL_IS_ENOUGH = true
+local FUEL_LEVEL_BATT_INSTANCE = 2 -- fuel level sensor in batt3 (instance 2)
+local FUEL_THRESHOLD = 30
 
 function update()
     local current_mode = vehicle:get_mode()
-    local fuel_threshold = 30
-    local fuel_val = battery:capacity_remaining_pct(2) -- fuel level sensor in batt3 (instance 2)
+    local fuel_val = battery:capacity_remaining_pct(FUEL_LEVEL_BATT_INSTANCE)
 
     -- FUEL LEVEL
-    if fuel_is_enough then
-        if fuel_val < fuel_threshold then
+    if FUEL_IS_ENOUGH then
+        if fuel_val < FUEL_THRESHOLD then
             gcs:send_text(MAV_SEVERITY.INFO, "-----")
             gcs:send_text(MAV_SEVERITY.INFO, "Low Fuel Failsafe, switch to RTL soon")
             gcs:send_text(MAV_SEVERITY.WARNING, string.format("Fuel %s", fuel_val))
             gcs:send_text(MAV_SEVERITY.INFO, "-----")
             notify:play_tune("MFT240L8 O3C O2B O3C O2B")
-            -- switch to RTL after t seconds under fuel_threshold
+            -- switch to RTL after t seconds under FUEL_THRESHOLD
             -- vehicle:set_mode(FLIGHT_MODE.RTL)
-            fuel_is_enough = false
+            FUEL_IS_ENOUGH = false
         end
     end
 
